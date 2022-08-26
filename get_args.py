3dd35cee177ee61d1f648097eef1d3dde99260a0
john12458 <john210628@gmail.com>
2022-08-26 14:48:14 +0800

    try deepfillv2

--- get_args.py
+++ get_args.py
@@ -2,29 +2,47 @@ import argparse
 def get_args(know_args=None):
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--note', dest='note', type=str, default="", help='note what you want')
-    # Mask Setting
+    """ Data Setting """
+    parser.add_argument('--imgsize', type = int, default = 256, help = 'size of image')
+    
+    """ Mask Setting """
     parser.add_argument('--mask_type', dest='mask_type', type=str, default="grid", help='grid, tri')
     parser.add_argument('--varmap_type', dest='varmap_type', type=str, default="notuse", help='notuse, var(warp), warp(var), small_grid')
     parser.add_argument('--varmap_threshold', dest='varmap_threshold', type=float, default=0.7, help='0 to 1 , if -1: not use')
-
+    # if ksize = 0 will calculate by sigma , see more detail on cv2. # kszie1 = cvRound(sigma1*(depth == CV_8U ? 3 : 4)*2 + 1)|1;  # 但有點不太懂為什麼是這個公式
+    parser.add_argument('--guassian_ksize', dest='guassian_ksize',type=int, default=5, help='guassian_blur kernel size') 
+    parser.add_argument('--guassian_sigma', dest='guassian_sigma',type=float, default=0.0, help='guassian_blur sigma')
     parser.add_argument('--guassian_blur', dest='guassian_blur',default=False, action="store_true", help='mask output with guassian_blur or not')
 
-    
-    # if ksize = 0 will calculate by sigma , see more detail on cv2.
-    # kszie1 = cvRound(sigma1*(depth == CV_8U ? 3 : 4)*2 + 1)|1;  # 但有點不太懂為什麼是這個公式
-    parser.add_argument('--guassian_ksize', dest='guassian_ksize',type=int, default=5, help='guassian_blur kernel size')
-    parser.add_argument('--guassian_sigma', dest='guassian_sigma',type=float, default=0.0, help='guassian_blur sigma')
-    
-    
-    
-    
-    # train Setting
-    parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
+    """ Train Setting """
+    parser.add_argument('--multi_gpu', type = bool, default = False, help = 'nn.Parallel needs or not')
+    parser.add_argument('--gpu_ids', type = str, default = "0,1", help = 'gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+    parser.add_argument('--cudnn_benchmark', type = bool, default = True, help = 'True for unchanged input data type')
+    parser.add_argument('--checkpoint_interval', type = int, default = 1, help = 'interval between model checkpoints')
+    parser.add_argument('--load_name', type = str, default = '', help = 'load model name')
+    parser.add_argument('--epochs', type = int, default = 40, help = 'number of epochs of training')
+    # parser.add_argument('--epoch', dest='epoch', type=int, default=200, help='# of epoch')
+    parser.add_argument('--resume', action='store_true')
+    parser.add_argument('--resume_epoch', type = int, default = 0)
     parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
-    parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for optimizer')
+    
+    # optimizer setting :
+    parser.add_argument('--lr_g', type = float, default = 2e-4, help = 'Adam: learning rate')
+    parser.add_argument('--lr_d', type = float, default = 2e-4, help = 'Adam: learning rate')
+    # parser.add_argument('--lr', dest='lr', type=float, default=0.0002, help='initial learning rate for optimizer')
+    parser.add_argument('--b1', type = float, default = 0.5, help = 'Adam: beta 1')
+    parser.add_argument('--b2', type = float, default = 0.999, help = 'Adam: beta 2')
+    parser.add_argument('--weight_decay', type = float, default = 0, help = 'Adam: weight decay')
+    parser.add_argument('--lr_decrease_epoch', type = int, default = 10, help = 'lr decrease at certain epoch and its multiple')
+    parser.add_argument('--lr_decrease_factor', type = float, default = 0.5, help = 'lr decrease factor, for classification default 0.1')
+    
+    # loss weight setting
+    parser.add_argument('--lambda_l1', type = float, default = 100, help = 'the parameter of L1Loss')
+    parser.add_argument('--lambda_perceptual', type = float, default = 10, help = 'the parameter of FML1Loss (perceptual loss)')
+    parser.add_argument('--lambda_gan', type = float, default = 1, help = 'the parameter of valid loss of AdaReconL1Loss; 0 is recommended')
+    parser.add_argument('--num_workers', type = int, default = 8, help = 'number of cpu threads to use during batch generation')
     
     # Model Setting
-    parser.add_argument('--backbone', dest='backbone', type=str, default="convnext_base_in22k", help='models in timm')
     
     parser.add_argument('--D_iter', dest='D_iter', type=int, default=5, help='d iter per batch')
     parser.add_argument('--G_iter', dest='G_iter', type=int, default=1, help='g iter per batch')
@@ -36,15 +54,33 @@ def get_args(know_args=None):
     parser.add_argument('--in_out_area_split', dest='in_out_area_split',default=False, action="store_true", help='split to in_area_mask_loss and out_area_mask_loss')
     parser.add_argument('--in_area_weight', dest='in_area_weight', type=float, default=0.5, help='in_area_mask weight')
     parser.add_argument('--out_area_weight', dest='out_area_weight', type=float, default=0.5, help='out_area_mask weight')
-
-    # Dir
+    
+    # Network parameters
+    # MaskEstimator
+    parser.add_argument('--backbone', dest='backbone', type=str, default="convnext_base_in22k", help='models in timm')
+    # InpaintGenerator from Deepfillv2
+    parser.add_argument('--in_channels', type = int, default = 4, help = 'input RGB image + 1 channel mask')
+    parser.add_argument('--out_channels', type = int, default = 3, help = 'output RGB image')
+    parser.add_argument('--latent_channels', type = int, default = 48, help = 'latent channels')
+    parser.add_argument('--pad_type', type = str, default = 'zero', help = 'the padding type')
+    parser.add_argument('--activation', type = str, default = 'lrelu', help = 'the activation type')
+    parser.add_argument('--norm', type = str, default = 'in', help = 'normalization type')
+    parser.add_argument('--init_type', type = str, default = 'xavier', help = 'the initialization type')
+    parser.add_argument('--init_gain', type = float, default = 0.02, help = 'the initialization gain')
+    
+    # Log Setting
     parser.add_argument('--data_dir',dest='data_dir',type=str, help="warppeData dir")
     # parser.add_argument('--train_dir',dest='train_dir',type=str,default="./data/celebAHQ/train_data",help="train dataset dir")
     # parser.add_argument('--val_dir',dest='val_dir',type=str,default="./data/celebAHQ/val_data",help="val dataset dir")
     parser.add_argument('--log_dir', dest='log_dir', default='./log/', help='log dir')
-   
-    # Other
     parser.add_argument('--wandb',default=False, action="store_true")
 
     args = parser.parse_args(know_args) if know_args != None else parser.parse_args()
     return args
+
+
+ 
+ 
+   
+    
+   
---
+++ main_deepfillv2.py
@@ -0,0 +1,334 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+from get_args import get_args
+""" Setting """
+wandb_prefix_name = "deep_fillv2"
+image_size = (256,256)
+seed = 5
+test_size = 0.1
+device = "cuda"
+weight_cliping_limit = 0.01
+know_args = None
+opt = get_args(know_args)
+if opt.multi_gpu == True:
+    os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu_ids
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import time
+import datetime
+import numpy as np
+import cv2
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.utils.data import DataLoader
+
+import models.deepfillv2.network as network
+import deepfillv2_utils
+from utils import (
+    seed_everything,
+    checkallData,
+    check_create_dir,
+    create_guassian_blur_f,
+    to_pillow_f,
+)
+from warp_dataset import WarppedDataset
+from natsort import natsorted
+from sklearn.model_selection import train_test_split
+import timm
+
+# 雖然寫WGAN 但我看起來像LSGAN...
+def WGAN_trainer(opt):
+    # ----------------------------------------
+    #      Initialize training parameters
+    # ----------------------------------------
+
+    # cudnn benchmark accelerates the network
+    cudnn.benchmark = opt.cudnn_benchmark
+    """ Wandb"""
+    if opt.wandb:
+        import wandb
+        wandb.init(project="warp_inpaint", entity='kycj')
+        opt.log_dir = f"{opt.log_dir}/{wandb.run.id}/"
+        wandb.config.update(opt)
+        wandb.run.name = f"{wandb_prefix_name}_{wandb.run.name}"
+        
+    print(vars(opt))
+
+    """ result dir setting """
+    checkpoint_dir = opt.log_dir + "/ckpts/"
+    check_create_dir(checkpoint_dir)
+    check_create_dir(checkpoint_dir+"/best/")
+
+    sample_dir = opt.log_dir + "/samples/"
+    check_create_dir(sample_dir)
+
+    """ Models """
+    # Generator
+    generator = network.GatedGenerator(opt)
+    print('Generator is created!')
+    network.weights_init(generator, init_type = opt.init_type, init_gain = opt.init_gain)
+    print('Initialize generator with %s type' % opt.init_type)
+    # Discrimnator
+    discriminator = network.PatchDiscriminator(opt)
+    print('Discriminator is created!')
+    network.weights_init(discriminator, init_type = opt.init_type, init_gain = opt.init_gain)
+    print('Initialize discriminator with %s type' % opt.init_type)
+    # percepturalnet
+    perceptualnet = network.PerceptualNet()
+    print('Perceptual network is created!')
+
+    """ Loss """
+    L1Loss = nn.L1Loss()
+    MSELoss = nn.MSELoss()
+
+    """ Optimizers """
+    optimizer_g = torch.optim.Adam(generator.parameters(), lr = opt.lr_g, betas = (opt.b1, opt.b2), weight_decay = opt.weight_decay)
+    optimizer_d = torch.optim.Adam(discriminator.parameters(), lr = opt.lr_d, betas = (opt.b1, opt.b2), weight_decay = opt.weight_decay)
+
+    # Learning rate decrease
+    def adjust_learning_rate(lr_in, optimizer, epoch, opt):
+        """Set the learning rate to the initial LR decayed by "lr_decrease_factor" every "lr_decrease_epoch" epochs"""
+        lr = lr_in * (opt.lr_decrease_factor ** (epoch // opt.lr_decrease_epoch))
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+    
+    # Save the two-stage generator model
+    def save_model_generator(net, epoch, opt):
+        """Save the model at "checkpoint_interval" and its multiple"""
+        model_name = 'deepfillv2_WGAN_G_epoch%d_batchsize%d.pth' % (epoch, opt.batch_size)
+        model_name = os.path.join(checkpoint_dir, model_name)
+        if opt.multi_gpu == True:
+            if epoch % opt.checkpoint_interval == 0:
+                torch.save(net.module.state_dict(), model_name)
+                print('The trained model is successfully saved at epoch %d' % (epoch))
+        else:
+            if epoch % opt.checkpoint_interval == 0:
+                torch.save(net.state_dict(), model_name)
+                print('The trained model is successfully saved at epoch %d' % (epoch))
+                
+    # Save the dicriminator model
+    def save_model_discriminator(net, epoch, opt):
+        """Save the model at "checkpoint_interval" and its multiple"""
+        model_name = 'deepfillv2_WGAN_D_epoch%d_batchsize%d.pth' % (epoch, opt.batch_size)
+        model_name = os.path.join(checkpoint_dir, model_name)
+        if opt.multi_gpu == True:
+            if epoch % opt.checkpoint_interval == 0:
+                torch.save(net.module.state_dict(), model_name)
+                print('The trained model is successfully saved at epoch %d' % (epoch))
+        else:
+            if epoch % opt.checkpoint_interval == 0:
+                torch.save(net.state_dict(), model_name)
+                print('The trained model is successfully saved at epoch %d' % (epoch))
+                
+    # load the model
+    def load_model(net, epoch, opt, type='G'):
+        """Save the model at "checkpoint_interval" and its multiple"""
+        if type == 'G':
+            model_name = 'deepfillv2_WGAN_G_epoch%d_batchsize%d.pth' % (epoch, opt.batch_size)
+        else:
+            model_name = 'deepfillv2_WGAN_D_epoch%d_batchsize%d.pth' % (epoch, opt.batch_size)
+        model_name = os.path.join(checkpoint_dir, model_name)
+        pretrained_dict = torch.load(model_name)
+        net.load_state_dict(pretrained_dict)
+
+    if opt.resume:
+        load_model(generator, opt.resume_epoch, opt, type='G')
+        load_model(discriminator, opt.resume_epoch, opt, type='D')
+        print('--------------------Pretrained Models are Loaded--------------------')
+        
+    # To device
+    if opt.multi_gpu == True:
+        generator = nn.DataParallel(generator)
+        discriminator = nn.DataParallel(discriminator)
+        perceptualnet = nn.DataParallel(perceptualnet)
+        generator = generator.cuda()
+        discriminator = discriminator.cuda()
+        perceptualnet = perceptualnet.cuda()
+    else:
+        generator = generator.cuda()
+        discriminator = discriminator.cuda()
+        perceptualnet = perceptualnet.cuda()
+    
+    # ----------------------------------------
+    #       Initialize training dataset
+    # ----------------------------------------
+    """ Data """
+    """ Train Val Split """
+    d_dir = f"{opt.data_dir}/{opt.mask_type}/"
+    origin_dir = f"{d_dir}/origin/"
+    print(origin_dir)
+    image_names = natsorted(os.listdir(origin_dir))
+    image_id_list = list(map(lambda s: s.split('.')[0], image_names))
+    print(len(image_id_list))
+
+    checkallData(d_dir,image_id_list)
+    print("Seed:",seed)
+    train_ids, valid_ids = train_test_split(image_id_list , test_size=test_size, random_state=seed)
+    print("Total train data:",len(train_ids))
+    print("Total valid data:", len(valid_ids))
+    guassian_blur_f = False
+    if opt.guassian_blur:
+        guassian_blur_f = create_guassian_blur_f(opt.guassian_ksize,opt.guassian_sigma)
+        print(f"Guassian_Blur ksize:{opt.guassian_ksize}, sigma:{opt.guassian_sigma}")
+    trainset = WarppedDataset(
+                 opt.data_dir,
+                 train_ids,
+                 opt.mask_type,
+                 opt.varmap_type,
+                 opt.varmap_threshold,
+                 guassian_blur_f=guassian_blur_f,
+                 transform=None, 
+                 return_mesh=True,
+                 checkExist=False,
+                 debug=False)
+    print("Total train len:",len(trainset))
+    train_loader = torch.utils.data.DataLoader(trainset, 
+                                          batch_size= opt.batch_size,
+                                          shuffle=True,
+                                          drop_last=True, 
+                                          num_workers=16
+                                          )
+    dataloader = train_loader
+    
+    
+    # ----------------------------------------
+    #            Training
+    # ----------------------------------------
+
+    # Initialize start time
+    prev_time = time.time()
+    
+    # Tensor type
+    Tensor = torch.cuda.FloatTensor
+
+    # Training loop
+    for epoch in range(opt.resume_epoch, opt.epochs):
+        for batch_idx, batch_data in enumerate(dataloader):
+            #  (img, height, width) = batch_data
+            # img = img.cuda()
+            # set the same free form masks for each batch
+            # mask = torch.empty(img.shape[0], 1, img.shape[2], img.shape[3]).cuda()
+            # for i in range(opt.batch_size):
+                # mask[i] = torch.from_numpy(train_dataset.InpaintDataset.random_ff_mask(
+                                                # shape=(height[0], width[0])).astype(np.float32)).cuda()
+            
+            origin_imgs, warpped_imgs, origin_meshes, warpped_meshes, masks = batch_data
+            masks = masks.permute(0,3,1,2)
+            origin_imgs, warpped_imgs = origin_imgs.to(device), warpped_imgs.to(device)
+            masks = masks.float().to(device)
+            
+            # deepfillv2 0:unmask 1:mask, 跟原本 mask 0:mask, 1: unmask不一樣
+            img, mask = origin_imgs, (1 - masks)
+
+            # LSGAN vectors
+            valid = Tensor(np.ones((img.shape[0], 1, opt.imgsize//32, opt.imgsize//32)))
+            fake = Tensor(np.zeros((img.shape[0], 1, opt.imgsize//32, opt.imgsize//32)))
+            zero = Tensor(np.zeros((img.shape[0], 1, opt.imgsize//32, opt.imgsize//32)))
+            # valid = Tensor(np.ones((img.shape[0], 1, height[0]//32, width[0]//32)))
+            # fake = Tensor(np.zeros((img.shape[0], 1, height[0]//32, width[0]//32)))
+            # zero = Tensor(np.zeros((img.shape[0], 1, height[0]//32, width[0]//32)))
+
+            ### Train Discriminator
+            optimizer_d.zero_grad()
+
+            # Generator output
+            first_out, second_out = generator(img, mask)
+
+            # forward propagation
+            first_out_wholeimg = img * (1 - mask) + first_out * mask        # in range [0, 1]
+            second_out_wholeimg = img * (1 - mask) + second_out * mask      # in range [0, 1]
+
+            # Fake samples
+            fake_scalar = discriminator(second_out_wholeimg.detach(), mask)
+            # True samples
+            true_scalar = discriminator(img, mask)
+
+            
+            # Loss and optimize
+            loss_fake = -torch.mean(torch.min(zero, -valid-fake_scalar))
+            loss_true = -torch.mean(torch.min(zero, -valid+true_scalar))
+            # Overall Loss and optimize
+            loss_D = 0.5 * (loss_fake + loss_true)
+            loss_D.backward()
+            optimizer_d.step()
+
+            ### Train Generator
+            optimizer_g.zero_grad()
+
+            # L1 Loss
+            first_L1Loss = (first_out - img).abs().mean()
+            second_L1Loss = (second_out - img).abs().mean()
+            
+            # GAN Loss
+            fake_scalar = discriminator(second_out_wholeimg, mask)
+            GAN_Loss = -torch.mean(fake_scalar)
+
+            # Get the deep semantic feature maps, and compute Perceptual Loss
+            img_featuremaps = perceptualnet(img)                          # feature maps
+            second_out_featuremaps = perceptualnet(second_out)
+            second_PerceptualLoss = L1Loss(second_out_featuremaps, img_featuremaps)
+
+            # Compute losses
+            loss = opt.lambda_l1 * first_L1Loss + opt.lambda_l1 * second_L1Loss + \
+                   opt.lambda_perceptual * second_PerceptualLoss + opt.lambda_gan * GAN_Loss
+            loss.backward()
+            optimizer_g.step()
+
+            # Determine approximate time left
+            batches_done = epoch * len(dataloader) + batch_idx
+            batches_left = opt.epochs * len(dataloader) - batches_done
+            time_left = datetime.timedelta(seconds = batches_left * (time.time() - prev_time))
+            prev_time = time.time()
+
+            """ LOG """
+            if opt.wandb:
+                log_dict = {
+                    "train":{
+                        "first_Mask_L1_Loss":  first_L1Loss.item(),
+                        "second_Mask_L1_Loss": second_L1Loss.item(),
+                        "Perceptual_Loss": second_PerceptualLoss.item(),
+                        "epoch": epoch 
+                    },
+                    "val":{}
+                }
+                wandb.log(log_dict,commit=False)
+
+
+            # Print log
+            print("\r[Epoch %d/%d] [Batch %d/%d] [first Mask L1 Loss: %.5f] [second Mask L1 Loss: %.5f]" %
+                ((epoch + 1), opt.epochs, batch_idx, len(dataloader), first_L1Loss.item(), second_L1Loss.item()))
+            print("\r[D Loss: %.5f] [G Loss: %.5f] [Perceptual Loss: %.5f] time_left: %s" %
+                (loss_D.item(), GAN_Loss.item(), second_PerceptualLoss.item(), time_left))
+            
+            masked_img = img * (1 - mask) + mask
+            mask = torch.cat((mask, mask, mask), 1)
+            if (batch_idx + 1) % 40 == 0:
+                img_list = [img, mask, masked_img, first_out, second_out]
+                name_list = ['gt', 'mask', 'masked_img', 'first_out', 'second_out']
+                img_path =  deepfillv2_utils.save_sample_png(sample_folder = sample_dir, sample_name = 'epoch%d' % (epoch + 1), img_list = img_list, name_list = name_list, pixel_max_cnt = 255)
+                print(img_path)
+                if opt.wandb:
+                    wandb.log({"Sample_Image_Train": wandb.Image(img_path)} , commit=False)
+            if opt.wandb:
+                wandb.log({} , commit=True)
+
+        # Learning rate decrease
+        adjust_learning_rate(opt.lr_g, optimizer_g, (epoch + 1), opt)
+        adjust_learning_rate(opt.lr_d, optimizer_d, (epoch + 1), opt)
+
+        # Save the model
+        save_model_generator(generator, (epoch + 1), opt)
+        save_model_discriminator(discriminator, (epoch + 1), opt)
+
+        ### Sample data every epoch
+        if (epoch + 1) % 1 == 0:
+            img_list = [img, mask, masked_img, first_out, second_out]
+            name_list = ['gt', 'mask', 'masked_img', 'first_out', 'second_out']
+            deepfillv2_utils.save_sample_png(sample_folder = sample_dir, sample_name = 'epoch%d' % (epoch + 1), img_list = img_list, name_list = name_list, pixel_max_cnt = 255)
+
+if __name__ == "__main__":
+    WGAN_trainer(opt)
